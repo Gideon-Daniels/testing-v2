aa9f193652b0d268b960b2b57be74cf8735bb9fe
Dragoonix <gideondaniels.dragoonix@gmail.com>
2021-06-08 10:13:03 +0200

    adding numbers

--- main.py
+++ main.py
@@ -2,3 +2,15 @@
 
 # we are testing our actual user requirements
 
+class MyMiniCalculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
